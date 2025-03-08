05f66e66ecaf0c394aa887d1e70b0e7fced83cc3
Ramesh Kannan R <rameshr@syncfusion.com>
2025-03-08 20:09:48 +0530

    Added Home Page

---
+++ src/app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from "next";
+import Link from 'next/link';
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Ramesh Kannan",
+  description: "Senior .NET Full Stack developer",
+};
+
+export const Navbar = () => {
+  return (
+    <nav className="p-4 text-black shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold">RRK</Link>
+        <div className="flex space-x-4">
+          {/* <Link href="/about" className="hover:underline">About</Link>
+          <Link href="/contact" className="hover:underline">Contact</Link> */}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;500;700&display=swap"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* <Navbar /> */}
+
+        <div className="container mx-auto mt-4 p-4">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
