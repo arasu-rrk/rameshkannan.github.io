9d451368b815957a6b2b525b2765c7ab0c93e1db
Ramesh Kannan Rajendran <rrk@Rameshs-MacBook-Pro.local>
2026-03-03 01:51:26 +0530

    Apply homepage background only on home page; remove from global body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import TypingAnimation from '../components/TypingAnimation';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="home-background grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-7xl gray-800">Hello, I&apos;m</h1>
         <TypingAnimation />
